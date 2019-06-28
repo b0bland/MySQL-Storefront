6a2920bc39d5989df3441f67db7f991aa3748876
b0bland <alfredychan96@gmail.com>
2019-06-27 23:01:55 -0700

    Added table with items

--- customer.js
+++ customer.js
@@ -9,3 +9,33 @@ var connection = mysql.createConnection({
     password: "password",
     database: "storeDB"
 });
+
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    storefront();
+    connection.end();
+});
+var table = new Table({
+    head: ['Item','Category','Price','Stock'],
+    colWidths: [20,20,15,10]
+});
+
+var storefront = function() {
+    connection.query("SELECT * FROM shop", function(err,res) {
+        if (err) throw err;
+        // console.log(res);
+        for (n=0;n<res.length;n++) {
+            var prod = res[n].product;
+            var dep = res[n].department;
+            var price = res[n].price;
+            var stock = res[n].stock;
+
+            table.push(
+                [prod,dep,price,stock]
+            )
+        };
+        console.log(table.toString());
+    })
+}
